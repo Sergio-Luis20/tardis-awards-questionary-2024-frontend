a7b50273e539ab2daac50d087a78b7925f070633
Sergio-Luis20 <sergioluisdasilvaruiz05@gmail.com>
2024-12-10 07:07:07 -0300

    back to normal

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000
+    }
   }
 })
